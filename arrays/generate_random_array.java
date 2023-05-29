0f749e3ead99bf9753f401d052d5fc0bed31e4e4
yagarwal0 <49115882+yagarwal0@users.noreply.github.com>
2023-05-29 13:32:46 +0530

    added range... now it includes the max and min feature

--- arrays/generate_random_array.java
+++ arrays/generate_random_array.java
@@ -1,8 +1,8 @@
-public static int[] generate_random_array(int range, int n){
+public static int[] generate_random_array(int range, int max, int min, int n){
         Random random = new Random();
         int temp_arr[] = new int[n];
         for(int i=0; i<n; i++){
-            temp_arr[i] = random.nextInt(range);
+            temp_arr[i] = random.nextInt(max + min) - min;
         }
         return temp_arr;
 }
